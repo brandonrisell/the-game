e959affd86e64863ce4a582d24a68967d7f15086
hghmn <nathanwhughes@gmail.com>
2015-07-24 11:38:32 -0700

    basic views using handlebars

--- controllers/game.js
+++ controllers/game.js
@@ -2,7 +2,5 @@ var fs = require('fs');
 
 /* routes for the game */
 exports.load = function(req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write("game time!");
-	res.end();
+	res.render('index', { title: 'The Game' });
 };
